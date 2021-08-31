759cbf820457d36d78428bdb435b0007627006d7
aguirre10 <aguirrejhensser@gmail.com>
2021-08-30 19:40:42 -0600

    Subiendo Proyecto Empresa Laravel

---
+++ resources/views/empleado/form.blade.php
@@ -0,0 +1,59 @@
+@php
+use App\Http\Controllers\PuestoController; 
+@endphp
+<div class="box box-info padding-1">
+    <div class="box-body">
+        
+        <div class="form-group">
+            {{ Form::label('codigo') }}
+            {{ Form::text('codigo', $empleado->codigo, ['class' => 'form-control' . ($errors->has('codigo') ? ' is-invalid' : ''), 'placeholder' => 'Codigo']) }}
+            {!! $errors->first('codigo', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('nombres') }}
+            {{ Form::text('nombres', $empleado->nombres, ['class' => 'form-control' . ($errors->has('nombres') ? ' is-invalid' : ''), 'placeholder' => 'Nombres']) }}
+            {!! $errors->first('nombres', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('apellidos') }}
+            {{ Form::text('apellidos', $empleado->apellidos, ['class' => 'form-control' . ($errors->has('apellidos') ? ' is-invalid' : ''), 'placeholder' => 'Apellidos']) }}
+            {!! $errors->first('apellidos', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('direccion') }}
+            {{ Form::text('direccion', $empleado->direccion, ['class' => 'form-control' . ($errors->has('direccion') ? ' is-invalid' : ''), 'placeholder' => 'Direccion']) }}
+            {!! $errors->first('direccion', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('telefono') }}
+            {{ Form::text('telefono', $empleado->telefono, ['class' => 'form-control' . ($errors->has('telefono') ? ' is-invalid' : ''), 'placeholder' => 'Telefono']) }}
+            {!! $errors->first('telefono', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('fecha_nacimiento') }}
+            {{ Form::date('fecha_nacimiento', $empleado->fecha_nacimiento, ['class' => 'form-control' . ($errors->has('fecha_nacimiento') ? ' is-invalid' : ''), 'placeholder' => 'Fecha Nacimiento']) }}
+            {!! $errors->first('fecha_nacimiento', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('id_puesto') }}
+            <div class="form-group col-md-6">
+            <select id="id_puesto" required="" name="id_puesto" class="form-control">
+                <option value="">--Select--</option>
+                {{$lista = PuestoController::dropBox()}}
+                @foreach ($lista as $nombre)
+                <option value="{{$nombre->id}}">{{$nombre->puesto}}</option>
+                @endforeach
+            </select>
+
+            
+ 
+            </div>
+
+            {!! $errors->first('id_puesto', '<div class="invalid-feedback">:message</p>') !!}
+        </div>
+
+    </div>
+    <div class="box-footer mt20">
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </div>
+</div>
